73d8e3d4cae17765bb32ec85a69afd3ba9f3add1
Bante Amlak <banteamlak1888@gmail.com>
2025-10-20 10:23:35 +0300

    i added new function

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const a=()=>{
     console.log("Function a aster");
 }
 a();                                                        
+
+
+const addisu =()=>{
+    console.log("Function addisu aster");
+}
